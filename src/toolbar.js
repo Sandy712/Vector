52e5845c5afba7a0f489c200bc33a22e9a95f57d
Sandy712 <sandeepprajapat200@gmail.com>
2024-06-21 12:41:24 +0530

    last

--- src/toolbar.js
+++ src/toolbar.js
@@ -11,6 +11,11 @@ export const PipelineToolbar = () => {
                 <DraggableNode type='llm' label='LLM' />
                 <DraggableNode type='customOutput' label='Output' />
                 <DraggableNode type='text' label='Text' />
+                <DraggableNode type='custom' label='custom_1'/>
+                <DraggableNode type='custom' label='custom_2'/>
+                <DraggableNode type='custom' label='custom_3'/>
+                <DraggableNode type='custom' label='custom_4'/>
+                <DraggableNode type='custom' label='custom_5'/>
             </div>
         </div>
     );
